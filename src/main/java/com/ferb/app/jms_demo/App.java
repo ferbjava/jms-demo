f7d5c2b3123a9363f4bd9afe7f32c35d20123108
Michal Kotecki <michal.kotecki@capgemini.com>
2019-11-06 14:45:07 +0100

    first commit

---
+++ src/main/java/com/ferb/app/jms_demo/App.java
@@ -0,0 +1,13 @@
+package com.ferb.app.jms_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
